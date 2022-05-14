9178c13da8a34b3b68a6c3b347fd6cfe1bc948a5
alamovAsadbek <104184218+alamovAsadbek@users.noreply.github.com>
2022-05-14 12:39:28 +0500

    Update Layout.js

--- src/Layout.js
+++ src/Layout.js
@@ -1,12 +1,17 @@
 import { Outlet, Link } from "react-router-dom";
 import "./sass.scss"
 const Layout = () => {
+    function play() {
+        var audio = document.getElementById("audio");
+        audio.play();
+    }
     return (
         <>
             <div className="navbar">
-                <Link to="/">Home</Link>
-                <Link to="/about">About Us</Link>
-                <Link to="/location">Location</Link>
+                <audio id="audio" src="https://www.fesliyanstudios.com/play-mp3/387"></audio>
+                <Link to="/" onClick={play}>Home</Link>
+                <Link to="/about" onClick={play}>About Us</Link>
+                <Link to="/location" onClick={play}>Location</Link>
             </div>
             <Outlet />
         </>
